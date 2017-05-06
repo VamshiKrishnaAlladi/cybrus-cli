3bd72456bee3c31f9783351303a7b535f33a9ffe
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-05-06 10:39:13 +0530

    refactored the fn expression to arrow fn

--- src/cybrus.js
+++ src/cybrus.js
@@ -1,7 +1,7 @@
 const chalk = require( 'chalk' );
 
 module.exports = {
-    'answer': function ( { petition = 'cybrus, please answer.' } ) {
+    'answer': ( { petition = 'cybrus, please answer.' } ) => {
         console.log( chalk.yellow( petition ) );
         return petition;
     }

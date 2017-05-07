bf5bbcfd0f15c166c8d464f50cc01ef6db7cfd35
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-05-07 14:16:36 +0530

    Renamed index.js to cybrus.js

--- src/cybrus.js
+++ src/cybrus.js
@@ -1,8 +1,23 @@
-const chalk = require( 'chalk' );
+#!/usr/bin/env node
 
-module.exports = {
-    'answer': ( { petition = 'cybrus, please answer.' } ) => {
-        console.log( chalk.yellow( petition ) );
-        return petition;
-    }
-};
+'use strict';
+
+const program = require( 'commander' );
+const actions = require( './actions/actions' );
+
+program
+.version( '0.0.1' )
+.command( 'answer' )
+.description( 'This command is used to make cybrus answer your intellectual questions.' )
+.option(
+    '-r, --request <request>',
+    'overwrites the request to cybrus. [default: "cybrus, please answer."]',
+    'cybrus, please answer.'
+)
+.action( actions.answer );
+
+program.parse( process.argv );
+
+if ( !process.argv.slice( 2 ).length ) {
+    program.outputHelp();
+}

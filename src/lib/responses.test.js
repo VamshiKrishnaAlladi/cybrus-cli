4f2b7173a49bfa3c667255195d7d15ddaf2f8aa0
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-05-31 00:11:46 +0530

    Updated responses tests to have a describe block

--- src/lib/responses.test.js
+++ src/lib/responses.test.js
@@ -1,10 +1,12 @@
-/* global test expect */
+/* global describe test expect */
 
 const responses = require( './responses' );
 
-test( 'responses.getRandomResponse() should return a non-empty string', () => {
-    const response = responses.getRandomResponse();
+describe( 'responses.getRandomResponse() :', () => {
+    test( 'should return a non-empty string', () => {
+        const response = responses.getRandomResponse();
 
-    expect( response ).toBeTruthy();
-    expect( typeof response ).toBe( 'string' );
+        expect( response ).toBeTruthy();
+        expect( typeof response ).toBe( 'string' );
+    } );
 } );

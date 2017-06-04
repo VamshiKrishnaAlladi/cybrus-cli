626f7fd16f48458f7f6d62f8e8af67d6370511a7
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-06-04 12:35:20 +0530

    moved configuration out of main function

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,65 @@
 'use strict';
 
+const gruntConfig = {
+    'eslint': {
+        'options': {
+            'config': '.eslintrc.json',
+            'fix': true
+        },
+        'target': {
+            'src': [
+                'src/**/*.js',
+                '*.js'
+            ]
+        }
+    },
+    'watch': {
+        'options': {
+            'interrupt': true,
+            'atBegin': true
+        },
+        'build': {
+            'files': [
+                'src/**/*.js',
+                '*.js'
+            ],
+            'tasks': [ 'build' ]
+        },
+        'build+': {
+            'files': [
+                'src/**/*.js',
+                '*.js'
+            ],
+            'tasks': [ 'build+' ]
+        },
+        'deploy': {
+            'files': [
+                'src/**/*.js',
+                '*.js'
+            ],
+            'tasks': [ 'deploy' ]
+        }
+    },
+    'shell': {
+        'options': {
+            'failOnError': true
+        },
+        'jest': {
+            'command': 'jest --colors'
+        },
+        'jestCov': {
+            'command': 'jest --colors --coverage'
+        },
+        'codecov': {
+            'command': 'codecov'
+        }
+    }
+};
+
 module.exports = function ( grunt ) {
     require( 'load-grunt-tasks' )( grunt );
 
-    grunt.initConfig( {
-        'eslint': {
-            'options': {
-                'config': '.eslintrc.json',
-                'fix': true
-            },
-            'target': {
-                'src': [
-                    'src/**/*.js',
-                    '*.js'
-                ]
-            }
-        },
-        'watch': {
-            'options': {
-                'interrupt': true,
-                'atBegin': true
-            },
-            'build': {
-                'files': [
-                    'src/**/*.js',
-                    '*.js'
-                ],
-                'tasks': [ 'build' ]
-            },
-            'build+': {
-                'files': [
-                    'src/**/*.js',
-                    '*.js'
-                ],
-                'tasks': [ 'build+' ]
-            },
-            'deploy': {
-                'files': [
-                    'src/**/*.js',
-                    '*.js'
-                ],
-                'tasks': [ 'deploy' ]
-            }
-        },
-        'shell': {
-            'options': {
-                'failOnError': true
-            },
-            'jest': {
-                'command': 'jest --colors'
-            },
-            'jestCov': {
-                'command': 'jest --colors --coverage'
-            },
-            'codecov': {
-                'command': 'codecov'
-            }
-        }
-    } );
+    grunt.initConfig( gruntConfig );
 
     grunt.registerTask( 'default', function () {
         grunt.log.ok( 'There are no default tasks specified, Please use a specific task.' +

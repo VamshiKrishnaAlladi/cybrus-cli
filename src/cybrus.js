707cefc6a69e2ddf0f1ec2254b787393d370db4d
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-06-11 23:05:44 +0530

    Wrapped the cybrus.js file contents in an iife

--- src/cybrus.js
+++ src/cybrus.js
@@ -1,23 +1,24 @@
 #!/usr/bin/env node
-
 'use strict';
 
-const program = require( 'commander' );
-const actions = require( './actions/actions' );
+( function () {
+    const program = require( 'commander' );
+    const actions = require( './actions/actions' );
 
-program
-.version( '0.0.1' )
-.command( 'answer' )
-.description( 'This command is used to make cybrus answer your intellectual questions.' )
-.option(
-    '-r, --request <request>',
-    'overwrites the request to cybrus. [default: "cybrus, please answer."]',
-    'cybrus, please answer.'
-)
-.action( actions.answer );
+    program
+    .version( '0.0.1' )
+    .command( 'answer' )
+    .description( 'This command is used to make cybrus answer your intellectual questions.' )
+    .option(
+        '-r, --request <request>',
+        'overwrites the request to cybrus. [default: "cybrus, please answer."]',
+        'cybrus, please answer.'
+        )
+    .action( actions.answer );
 
-program.parse( process.argv );
+    program.parse( process.argv );
 
-if ( !process.argv.slice( 2 ).length ) {
-    program.outputHelp();
-}
+    if ( !process.argv.slice( 2 ).length ) {
+        program.outputHelp();
+    }
+} )();

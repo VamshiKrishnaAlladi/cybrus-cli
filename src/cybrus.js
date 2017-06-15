ad71007e8e3bcc859c965fa56d5f4915ef3e1f58
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-06-15 16:51:02 +0530

    Modified to use the new sub-command structure for commander and Actions class

--- src/cybrus.js
+++ src/cybrus.js
@@ -1,22 +1,19 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 'use strict';
 
 ( function () {
     const program = require( 'commander' );
-    const actions = require( './actions/actions' );
+    const Actions = require( './actions/actions' );
+    const actions = new Actions(
+        require( 'chalk' ),
+        require( './lib/util' ),
+        require( './lib/responses' )
+    );
 
     program
     .version( '0.0.1' )
-    .command( 'answer' )
-    .description( 'This command is used to make cybrus answer your intellectual questions.' )
-    .option(
-        '-r, --request <request>',
-        'overwrites the request to cybrus. [default: "cybrus, please answer."]',
-        'cybrus, please answer.'
-    )
-    .action( actions.answer );
-
-    program.parse( process.argv );
+    .command( 'answer', 'This command is used to make cybrus answer your intellectual questions.' )
+    .parse( process.argv );
 
     if ( !process.argv.slice( 2 ).length ) {
         actions.startAConversation();

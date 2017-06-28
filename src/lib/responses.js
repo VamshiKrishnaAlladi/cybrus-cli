d1535d9b0119d2f5961842de62576b3d2543adb7
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-06-28 12:16:12 +0530

    Removed unnecessary generator code

--- src/lib/responses.js
+++ src/lib/responses.js
@@ -5,16 +5,8 @@ const responseStrings = require( './responseStrings.json' );
 
 const numberOfResponseStrings = responseStrings.length;
 
-function *getRandomResponsesGenerator () {
-    while ( true ) {
-        yield responseStrings[ utilities.getRandomNumber( numberOfResponseStrings ) ];
-    }
-}
-
-const responseGenerator = getRandomResponsesGenerator();
-
 function getRandomResponse () {
-    return responseGenerator.next().value;
+    return responseStrings[ utilities.getRandomNumber( numberOfResponseStrings ) ];
 }
 
 module.exports = { getRandomResponse };
